811c44a0e3b830ca8851ded358289e72978fad9a
tkotani1 <35750723+tkotani1@users.noreply.github.com>
2018-01-29 14:50:35 +0900

    add interactive support, add how to reinstall

--- autoconf/git_config.sh
+++ autoconf/git_config.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 if type "git" > /dev/null 2>&1; then
+    echo "Type in your name (or github account name - e.g. 'tkotani1'):"
+    read fullname
+    echo "Type in your email (e.g. '35750723+tkotani1@users.noreply.github.com'):"
+    read email
 
-    git config --global user.name "tkotani1"
-    git config --global user.email "35750723+tkotani1@users.noreply.github.com"
+    git config --global user.name $fullname
+    git config --global user.email $email
     git config --global core.editor 'vim -c "set fenc=utf-8"'
     git config --global color.diff auto
     git config --global color.status auto
@@ -12,7 +16,9 @@ if type "git" > /dev/null 2>&1; then
 else
 
     echo " [WARNING] 'git' command not found"
-    echo " Retry using $ sudo apt install git && sh install.sh"
+    echo " Run me again as follows:"
+    echo " $ sudo apt install git && sh install.sh"
     exit 1
 
 fi
+
