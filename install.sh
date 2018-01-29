c6e5ecc6d675e934bf07d7123ecec38d047fb34a
tkotani1 <35750723+tkotani1@users.noreply.github.com>
2018-01-29 14:38:38 +0900

    add interactive support for autoconf

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-THIS_DIR=`pwd`
-
-echo " - now git-completion files downloading..."
+echo " -- Favorite tools downloading..."
+# -- git-completion
 rURL=https://raw.githubusercontent.com
 curl ${rURL}/git/git/master/contrib/completion/git-completion.bash > .git-completion.bash
 curl ${rURL}/git/git/master/contrib/completion/git-prompt.sh > .git-prompt.sh
 
-echo " - now linking..."
+# -- cdhist
+curl http://www.unixuser.org/~euske/doc/bashtips/cdhist.sh > .cdhist.sh
+
+echo " -- Now linking..."
+THIS_DIR=`pwd`
 for f in .??*; do
     [ "$f" = ".git" ] && continue
     ln -snfv ${THIS_DIR}/"$f" ~/
 done
 
-echo " - install & configure"
-sh autoconf/apt_install.sh
-sh autoconf/git_config.sh
-sh autoconf/git_clone.sh
+echo " -- Run 'autoconf/*'? (y|n):"
+read ans
+if [ $ans == "y" ]; then
+    echo " -- Now autoconf booting..."
+    . ./autoconf/apt_install.sh
+    . ./autoconf/git_config.sh
+    . ./autoconf/git_clone.sh
+fi
 
-echo " - done!"
+echo " -- Done!"
+
