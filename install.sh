b26297d617be9e24c274e71f4b30d5f81af6da3f
tkotani1 <35750723+tkotani1@users.noreply.github.com>
2018-01-29 20:50:00 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+rundir=$(cd $(dirname $0) && pwd)
+cd $rundir
 
 echo " Now downloading..."
 # -- git-completion
 rURL=https://raw.githubusercontent.com
-curl ${rURL}/git/git/master/contrib/completion/git-completion.bash > .git-completion.bash
-curl ${rURL}/git/git/master/contrib/completion/git-prompt.sh > .git-prompt.sh
-
+curl -L ${rURL}/git/git/master/contrib/completion/git-completion.bash > .git-completion.bash
+curl -L ${rURL}/git/git/master/contrib/completion/git-prompt.sh > .git-prompt.sh
 # -- cdhist
-curl http://www.unixuser.org/~euske/doc/bashtips/cdhist.sh > .cdhist.sh
+curl -L http://www.unixuser.org/~euske/doc/bashtips/cdhist.sh > .cdhist.sh
+
 
 echo " Now linking..."
-THIS_DIR=`pwd`
+mkdir -p .vim/backup
+mkdir -p .vim/viminfo
 for f in .??*; do
     [ "$f" = ".git" ] && continue
-    ln -snfv ${THIS_DIR}/"$f" ~/
+    ln -snfv ${rundir}/"$f" ~/
 done
+
 
 echo " Run 'autoconf/*'? (y|n):"
 read ans
@@ -24,6 +28,7 @@ if [ $ans == "y" ]; then
     . ./autoconf/git_config.sh
     . ./autoconf/git_clone.sh
 fi
+
 
 echo " Done!"
 
