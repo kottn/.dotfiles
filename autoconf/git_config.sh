5bb802eaaa8fa81a49340b6b2e00c9d4ccae6d04
kottn <35750723+kottn@users.noreply.github.com>
2018-02-04 20:10:22 +0900

    Change default user.name

--- autoconf/git_config.sh
+++ autoconf/git_config.sh
@@ -2,8 +2,8 @@
 
 if type "git" > /dev/null 2>&1; then
 
-    git config --global user.name tkotani1
-    git config --global user.email 35750723+tkotani1@users.noreply.github.com
+    git config --global user.name kottn
+    git config --global user.email 35750723+kottn@users.noreply.github.com
     git config --global core.editor 'vim -c "set fenc=utf-8"'
     git config --global color.diff auto
     git config --global color.status auto
